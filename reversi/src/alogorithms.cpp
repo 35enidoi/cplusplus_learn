c102084a68cde9bf6cf4eaa1cc337b16fa015662
いおぢね <143810964+35enidoi@users.noreply.github.com>
2025-03-23 20:03:32 +0900

    uril.randintの仕様を変更

--- reversi/src/alogorithms.cpp
+++ reversi/src/alogorithms.cpp
@@ -7,8 +7,6 @@ ReversiPos ReversiAlgorithms::RandomSelect::select(std::vector<ReversiPos> posit
 ReversiPos ReversiAlgorithms::RandomSelect::static_select(std::vector<ReversiPos> positions) {
     if (positions.size() == 0) {
         throw ReversiException::AlgorithmSelectNoStone();
-    } else if (positions.size() == 1) {
-        return positions.at(0);
     } else {
         unsigned int select_pos = ReversiUtil::randint(0, positions.size() - 1);
         return positions.at(select_pos);
--- reversi/src/util.cpp
+++ reversi/src/util.cpp
@@ -4,6 +4,6 @@ int ReversiUtil::randint(int under_limit, int over_limit) {
     static std::random_device rd;
     static std::mt19937 gen(rd());
 
-    std::uniform_int_distribution<int> random_generate(under_limit, over_limit - 1);
+    std::uniform_int_distribution<int> random_generate(under_limit, over_limit);
     return random_generate(gen);
 }
